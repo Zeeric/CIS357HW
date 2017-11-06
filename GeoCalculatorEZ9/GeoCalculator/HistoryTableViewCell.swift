fe7ba534fc951042fa5e4e668d2cf3856790c989
Zeeric <zeinstrae@gmail.com>
2017-11-05 19:26:16 -0500

    HW 9

---
+++ GeoCalculatorEZ9/GeoCalculator/HistoryTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryTableViewCell.swift
+//  GeoCalculator
+//
+//  Created by X Code User on 11/1/17.
+//  Copyright © 2017 Jonathan Engelsma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HistoryTableViewCell: UITableViewCell {
+    @IBOutlet weak var destPoint: UILabel!
+    @IBOutlet weak var origPoint: UILabel!
+    @IBOutlet weak var timestamp: UILabel!
+}
