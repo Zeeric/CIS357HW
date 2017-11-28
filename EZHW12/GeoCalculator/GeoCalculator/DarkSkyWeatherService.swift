8ad424ac3d986a576e3a34f4d8b4cebf809c54e5
Zeeric <zeinstrae@gmail.com>
2017-11-28 17:17:22 -0500

    CIS 357 HW 12: Dark Sky API

---
+++ EZHW12/GeoCalculator/GeoCalculator/DarkSkyWeatherService.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+let sharedDarkSkyInstance = DarkSkyWeatherService()
+
+class DarkSkyWeatherService: WeatherService {
+    
+    let API_BASE = "https://api.darksky.net/forecast/"
+    var urlSession = URLSession.shared
+    
+    class func getInstance() -> DarkSkyWeatherService {
+        return sharedDarkSkyInstance
+    }
+    
+    func getWeatherForDate(date: Date, forLocation location: (Double, Double),
+                           completion: @escaping (Weather?) -> Void)
+    {
+        
+        let urlStr = API_BASE +  DARK_SKY_WEATHER_API_KEY + "/\(location.0),\(location.1),\(Int(date.timeIntervalSince1970))"
+        let url = URL(string: urlStr)
+        
+        let task = self.urlSession.dataTask(with: url!) {
+            (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let _ = response {
+                let parsedObj : Dictionary<String,AnyObject>!
+                do {
+                    parsedObj = try JSONSerialization.jsonObject(with: data!, options:
+                        .allowFragments) as? Dictionary<String,AnyObject>
+                    
+                    guard let currently = parsedObj["currently"],
+                        let summary = currently["summary"] as? String,
+                        let iconName = currently["icon"] as? String,
+                        let temperature = currently["temperature"] as? Double
+                        else {
+                            completion(nil)
+                            return
+                    }
+                    
+                    let weather = Weather(iconName: iconName, temperature: temperature,
+                                          summary: summary)
+                    completion(weather)
+                    
+                }  catch {
+                    completion(nil)
+                }
+            }
+        }
+        
+        task.resume()
+    }
+}
---
+++ EZHW12/GeoCalculator/GeoCalculator/Weather.swift
@@ -0,0 +1,17 @@
+import Foundation
+struct Weather {
+    var iconName : String
+    var temperature : Double
+    var summary : String
+    
+    init(iconName: String, temperature: Double, summary: String) {
+        self.iconName = iconName
+        self.temperature = temperature
+        self.summary = summary
+    }
+}
+
+protocol WeatherService {
+    func getWeatherForDate(date: Date, forLocation location: (Double, Double),
+                           completion: @escaping (Weather?) -> Void)
+}
